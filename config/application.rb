5c327b28ea267dcacf743fd439ca4f1e07d09362
tackernao0522 <cheap_trick_magic@yahoo.co.jp>
2022-04-03 20:50:59 +0900

    config/application.rbの修正

--- config/application.rb
+++ config/application.rb
@@ -41,7 +41,7 @@ module App
     config.middleware.use ActionDispatch::Cookies
 
     # Cookietのsamesite属性を変更する(Rails v6.1〜, :strict, :lax, :none)
-    config.action_despatch.cookies_same_site_protection = ENV["COOKIES_SAME_SITE"].to_sym if Rails.env.production?
+    config.action_dispatch.cookies_same_site_protection = ENV["COOKIES_SAME_SITE"].to_sym if Rails.env.production?
 
     config.api_only = true
   end
