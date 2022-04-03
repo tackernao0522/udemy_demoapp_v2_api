dd6889d811da0345a6bab72de3fe4734f22b8aa6
tackernao0522 <cheap_trick_magic@yahoo.co.jp>
2022-04-03 18:15:19 +0900

    82 認証を行うコントローラを作成

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,9 +1,2 @@
 class Api::V1::UsersController < ApplicationController
-  before_action :authenticate_active_user
-
-  def index
-    # users = User.all
-    # as_json => ハッシュの形でSONデータを返す { "id" => 1, "name" => "test", ... }
-    render json: current_user.as_json(only: [:id, :name, :email, :created_at])
-  end
 end
