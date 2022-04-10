72d73d2e2dbb851dc8b769716ebceddba226cd32
tackernao0522 <cheap_trick_magic@yahoo.co.jp>
2022-04-10 15:00:36 +0900

    102 RailsにSSLリダイレクト処理を行う(production.rb, application_controller.rbを編集)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::API
+  # 301リダイレクト(本番環境のみ有効)
+  before_action :moved_permanently, if: :is_redirect
   # Cookieを扱う
   include ActionController::Cookies
   # 認可を行う
@@ -19,5 +21,16 @@ class ApplicationController < ActionController::API
     # Internal Server Error
     def response_500(msg = "Internal Server Error")
       render status: 500, json: { status: 500, error: msg }
+    end
+
+    # リダイレクト条件に一致した場合はtrueを返す
+    def is_redirect
+      redirect_domain = "herokuapp.com"
+      Rails.env.production? && ENV["BASE_URL"] && request.host.include?(redirect_domain)
+    end
+
+    # 301リダイレクトを行う
+    def moved_permanently
+      redirect_to "#{ENV["BASE_URL"]}#{request.path}", status: 301
     end
 end
